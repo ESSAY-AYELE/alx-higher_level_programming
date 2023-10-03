484c98311b525b11de803308247b2c1befa8a6df
ESSAY-AYELE <misganaa352@gmail.com>
2023-10-03 16:58:22 -0500

    forteeth commit

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_node - nserts a number into a sorted singly linked list
+ * @head: the head of the linked list
+ * @number: the number to be inserted
+ * Return: the address of the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node;
+	listint_t *tmp;
+
+	if (head == NULL && *head == NULL)
+		return (NULL);
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = number;
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		if (tmp->next->n > number || tmp->next == NULL)
+		{
+			node->next = tmp->next;
+			tmp->next = node;
+			break;
+		}
+		tmp = tmp->next;
+	}
+	return (node);
+}
+
