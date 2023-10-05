43dddd89c7a5b7558479b5642f4a5ba4012164a2
ESSAY-AYELE <misganaa352@gmail.com>
2023-10-05 10:00:11 -0500

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
     length = len(argv)
-    print("{} arguments:".format(length - 1))
+    if length == 1:
+        tok = 's.'
+    elif length == 2:
+        tok = ':'
+    else:
+        tok = 's:'
+    print("{} argument{}".format(length - 1, tok))
     for i in range(1, length):
         print("{}: {}".format(i, argv[i]))
