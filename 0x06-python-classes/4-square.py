c23b4a18ab851abc45b92ac90ed56ea83d199cfa
ESSAY-AYELE <misganaa352@gmail.com>
2023-10-24 14:42:50 -0500

    4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" what ever """
+
+
+class Square:
+    """ class whaeve"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        """ computes the area of square """
+        return (self.__size ** 2)
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    @property
+    def size(self, v)
+    """getter method"""
+    return self.__size
+        
