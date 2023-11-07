f508aa9986e7d2bc97a3367a5f172c53febb1a01
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-07 12:02:20 -0600

    6

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,6 +8,7 @@
     You don’t need to manage file permission exceptions."""
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """save an object to text file"""
     with open(filename, mode="w") as file:
