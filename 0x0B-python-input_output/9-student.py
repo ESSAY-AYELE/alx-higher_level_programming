4bf993f08065e3c47ace9d15fec24a15cc9bbea3
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-07 13:26:59 -0600

    9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,9 +5,13 @@
         first_name
         last_name
         age
-    Instantiation with first_name, last_name and age: def __init__(self, first_name, last_name, age):
-    Public method def to_json(self): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py)
+    Instantiation with first_name, last_name and age:
+    def __init__(self, first_name, last_name, age):
+    Public method def to_json(self): that retrieves
+    a dictionary representation of a Student instance (same as 8-class_to_json.py)
     You are not allowed to import any module"""
+
+
 class Student:
     """ student class that represents"""
 
