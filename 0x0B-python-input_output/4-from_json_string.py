2f21fd24634c84e8ec0421577490fcc3ba969adf
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-07 11:30:15 -0600

    6

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import json
 """Write a function that returns an object
     (Python data structure) represented by a JSON string:
 
     Prototype: def from_json_string(my_str):
     You don’t need to manage exceptions
     if the JSON string doesn’t represent an object."""
-
+import json
 
 def from_json_string(my_str):
     """ change json to object"""
---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Write a function that writes an Object
+    to a text file, using a JSON representation:
+
+    Prototype: def save_to_json_file(my_obj, filename):
+    You must use the with statement
+    You don’t need to manage exceptions if the object can’t be serialized.
+    You don’t need to manage file permission exceptions."""
+import json
+
+def save_to_json_file(my_obj, filename):
+    """save an object to text file"""
+    with open(filename, mode="w") as file:
+        json.dump(my_obj, file)
