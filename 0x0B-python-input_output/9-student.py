1bd5fcfee3637c92f493ee2def4e5be2f4aed82c
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-07 13:25:51 -0600

    9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Write a class Student that defines a student by:
+
+    Public instance attributes:
+        first_name
+        last_name
+        age
+    Instantiation with first_name, last_name and age: def __init__(self, first_name, last_name, age):
+    Public method def to_json(self): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py)
+    You are not allowed to import any module"""
+class Student:
+    """ student class that represents"""
+
+    def __init__(self, first_name, last_name, age):
+        """ init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """retireves a dictionary the instance"""
+        return self.__dict__
