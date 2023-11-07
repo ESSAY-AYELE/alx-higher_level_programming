35f8e95ef840da0cab8d90fc3c8c0423e6e3964c
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-07 10:09:58 -0600

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,5 @@ def read_file(filename=""):
     """ functionread file"""
 
     with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(),end="")
     return
