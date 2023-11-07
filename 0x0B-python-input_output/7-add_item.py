b243997f103e581c23df18f7619cefdcb77d73f4
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-07 12:31:31 -0600

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,6 +9,7 @@
     in a file named add_item.json
     If the file doesn’t exist, it should be created
     You don’t need to manage file permissions / exceptions"""
+import sys
 
 
 if __name__ == "__main__":
