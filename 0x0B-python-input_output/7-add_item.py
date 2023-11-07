1efb675a26123ce3d0e595dc37aa807237e1c779
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-07 12:27:11 -0600

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,16 @@
     and then save them to a file:
 
     You must use your function save_to_json_file from 5-save_to_json_file.py
-    You must use your function load_from_json_file from 6-load_from_json_file.py
-    The list must be saved as a JSON representation in a file named add_item.json
+    You must use your function load_from_json_file
+    from 6-load_from_json_file.py
+    The list must be saved as a JSON representation
+    in a file named add_item.json
     If the file doesn’t exist, it should be created
     You don’t need to manage file permissions / exceptions"""
 
 
 if __name__ == "__main__":
-     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
