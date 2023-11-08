d5655562ed49726249d611f9aeedf52f3eac3137
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-08 07:02:17 -0600

    4

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" fuction check if object is like a class"""
+
+
+def is_same_class(obj, a_class):
+    """ the function isinstance of a class"""
+    return isinstance(obj, a_class)
